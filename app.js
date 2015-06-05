fb634802eeb9327dd93931882e4d2fb620c47554
oliver <oliverw56#gmail.com>
2015-06-05 15:14:25 -0400

    changed factory to make it work again

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 var App = angular.module('App', []);
 
-App.factory('userFactory', userFactory);
-
-function userFactory() {
-
-    function _load() {
-        // load logic here
-        _users = [ { first: 'jay', last: 'desm' } ];
-    }
-
+App.factory('userFactory', function() {
 
     // some private variables
     var _users = [];
+/*
+  //I want to use this but it was giving errors about a semi colon when i had it included?
     var _service {
-        // model data
-        model: { users: _users },
-        load:  _load
-    }
-    return _service; // exposes _service which contains references
+       // model data
+       model: { users: _users },
+       load:  _load
+    };
+*/   
+
+     function _load() {
+        // load logic here
+     var users = [  { first: "john", last: "doe" } ];
+        //once this is working, get data from external find.
 }
+     return users; // exposes _service which contains references
+});
 
 
 App.controller('appCtrl',['$scope','$http', function($scope, $http) {
@@ -27,6 +28,9 @@ App.controller('appCtrl',['$scope','$http', function($scope, $http) {
         .then(function(res){
             $scope.list = res.data;
         });
+
+//I think this line isn't correct, so keeping it as a comment till i figure out syntax.
+//$scope.list = userFactory.users;
 
 $scope.showBool = false;
 $scope.button= {text:"load"};
